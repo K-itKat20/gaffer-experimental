64ba028fbec2ffdff400b1ac07e60c7e3e785903
Rocky <rocky341@protonmail.com>
2021-04-08 10:47:12 +0100

    redirects to keycloak

--- gaas-ui/src/index.tsx
+++ gaas-ui/src/index.tsx
@@ -7,7 +7,12 @@ import { CssBaseline } from "@material-ui/core";
 import { StylesProvider } from "@material-ui/styles";
 import GlobalStyles from "../src/components/GlobalStyles";
 import "./styles/_App.scss";
+import Keycloak from "keycloak-js";
 
+// eslint-disable-next-line new-cap
+const keycloak: Keycloak.KeycloakInstance = Keycloak();
+keycloak.init({ onLoad: "login-required" })
+  .success(() => {
 ReactDOM.render(
     <React.StrictMode>
         <StylesProvider injectFirst>
@@ -19,7 +24,8 @@ ReactDOM.render(
         </StylesProvider>
     </React.StrictMode>,
     document.getElementById("root")
-);
+)})
+.error((error) => console.log(error));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
