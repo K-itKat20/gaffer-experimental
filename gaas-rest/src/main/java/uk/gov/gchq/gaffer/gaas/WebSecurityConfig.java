4ddf8759ea69ad783e1c31a3928a1024cd98ac95
“Go159lab” <go159lab@tutanota.com>
2021-04-16 15:14:03 +0100

    add keycloak role based authentication

--- gaas-rest/src/main/java/uk/gov/gchq/gaffer/gaas/WebSecurityConfig.java
+++ gaas-rest/src/main/java/uk/gov/gchq/gaffer/gaas/WebSecurityConfig.java
@@ -65,7 +65,7 @@ class WebSecurityConfig extends KeycloakWebSecurityConfigurerAdapter {
      * Secure appropriate endpoints
      */
     @Override
-    protected void configure(HttpSecurity http) throws Exception {
+    protected void configure(final HttpSecurity http) throws Exception {
 
         super.configure(http);
         http.headers().frameOptions().sameOrigin();
@@ -76,7 +76,7 @@ class WebSecurityConfig extends KeycloakWebSecurityConfigurerAdapter {
                 .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
                 .and()
                 .authorizeRequests();
-        expressionInterceptUrlRegistry = expressionInterceptUrlRegistry.antMatchers("/graphs/*, /v2/api-docs", "/swagger-ui.html", "/swagger-ui/", "/swagger-ui/**", "/swagger-resources","/swagger-resources/**", "/webjars/**").hasRole("user");
+        expressionInterceptUrlRegistry = expressionInterceptUrlRegistry.antMatchers("/graphs/*, /v2/api-docs", "/swagger-ui.html", "/swagger-ui/", "/swagger-ui/**", "/swagger-resources", "/swagger-resources/**", "/webjars/**").hasRole("user");
         expressionInterceptUrlRegistry.anyRequest().permitAll();
     }
 }
