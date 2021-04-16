6e8890c9ebbbe04aae4946994fd184ba1bb7cb94
“Go159lab” <go159lab@tutanota.com>
2021-04-16 15:05:13 +0100

    add keycloak role based authentication

--- gaas-rest/src/main/java/uk/gov/gchq/gaffer/gaas/WebSecurityConfig.java
+++ gaas-rest/src/main/java/uk/gov/gchq/gaffer/gaas/WebSecurityConfig.java
@@ -26,15 +26,12 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.ExpressionUrlAuthorizationConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.authority.mapping.SimpleAuthorityMapper;
 import org.springframework.security.core.session.SessionRegistryImpl;
 import org.springframework.security.web.authentication.session.RegisterSessionAuthenticationStrategy;
 import org.springframework.security.web.authentication.session.SessionAuthenticationStrategy;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.CorsConfigurationSource;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-import java.util.Arrays;
-import static org.springframework.security.config.Customizer.withDefaults;
 
 @Configuration
 @EnableWebSecurity
@@ -63,25 +60,23 @@ class WebSecurityConfig extends KeycloakWebSecurityConfigurerAdapter {
         return new RegisterSessionAuthenticationStrategy(
                 new SessionRegistryImpl());
     }
-    @Bean
-    CorsConfigurationSource corsConfigurationSource() {
-        CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(Arrays.asList("*"));
-        configuration.setAllowedMethods(Arrays.asList("GET", "POST", "DELETE", "OPTIONS"));
-        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-        source.registerCorsConfiguration("/**", configuration);
-        return source;
-    }
 
+    /**
+     * Secure appropriate endpoints
+     */
     @Override
-    protected void configure(final HttpSecurity http) throws Exception {
+    protected void configure(HttpSecurity http) throws Exception {
+
         super.configure(http);
-        http.csrf().disable()
-                .authorizeRequests()
-                .antMatchers("/v2/api-docs", "/swagger-ui.html", "/swagger-ui/", "/swagger-ui/**", "/swagger-resources",
-                        "/swagger-resources/**", "/webjars/**")
-                .permitAll()
-                .anyRequest().authenticated();
-        http.cors(withDefaults());
+        http.headers().frameOptions().sameOrigin();
+
+        ExpressionUrlAuthorizationConfigurer<HttpSecurity>.ExpressionInterceptUrlRegistry expressionInterceptUrlRegistry = http.cors()
+                .and()
+                .csrf().disable()
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
+                .authorizeRequests();
+        expressionInterceptUrlRegistry = expressionInterceptUrlRegistry.antMatchers("/graphs/*, /v2/api-docs", "/swagger-ui.html", "/swagger-ui/", "/swagger-ui/**", "/swagger-resources","/swagger-resources/**", "/webjars/**").hasRole("user");
+        expressionInterceptUrlRegistry.anyRequest().permitAll();
     }
 }
